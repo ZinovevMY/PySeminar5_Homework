b3c4fcca7ca4fbf4bdf379037a11ee04d187c680
Mikhail Zinovev <mehan@list.ru>
2022-12-09 12:24:06 +0300

    add functions

--- Task 2.py
+++ Task 2.py
@@ -11,8 +11,29 @@ import random
 
 
 candy_count = 2021
+player1_name = input("Введите имя первого игрока: ")
+player2_name = input("Введите имя второго игрока: ")
 player1 = 0
 player2 = 0
+player1_moves_log = list()
+player2_moves_log = list()
+
+
+def take_input(player_name: str):
+    while True:
+        value = int(input(f"Сколько конфет берет {player_name}?: "))
+        if not (value in range(1, 29)):
+            print("Количество конфет должно быть от 1 до 28! Повторите!")
+            continue
+        # if value > candy_count:
+        #     print("Нет столько конфет на столе!")
+        #     continue
+        return value
+
+def check_winner(player_name: str):
+    if candy_count < 28:
+        print(f"Игрок {player_name} победил!!!")
+
 print("Начинаем игру!")
 whose_move = random.randint(1, 3)
 
